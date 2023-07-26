5f5666353908b88f0038f8dad9179378c8ae8527
Yatendra <yatendra121kushwaha@gmail.com>
2023-07-26 13:42:04 +0530

    test build

--- packages/winston/dist/winston.mjs
+++ packages/winston/dist/winston.mjs
@@ -1,12 +1,12 @@
-import { transports as o, format as e, createLogger as l } from "winston";
-import { existsSync as n, mkdirSync as a } from "fs";
+import { transports as o, format as e, createLogger as i } from "winston";
+import { existsSync as a, mkdirSync as l } from "fs";
 import "path";
 import "winston-daily-rotate-file";
-const s = "logs";
-let t = s;
-n(t) || a(t);
-const i = "debug", r = new o.DailyRotateFile({
-  level: i,
+const n = "logs";
+let t = n;
+a(t) || l(t);
+const s = "debug", r = new o.DailyRotateFile({
+  level: s,
   filename: t + "/%DATE%.log",
   datePattern: "YYYY-MM-DD",
   zippedArchive: !0,
@@ -14,12 +14,12 @@ const i = "debug", r = new o.DailyRotateFile({
   maxSize: "20m",
   maxFiles: "14d",
   format: e.combine(e.errors({ stack: !0 }), e.timestamp(), e.json())
-}), m = l({
+}), m = i({
   transports: [
-    new o.Console({
-      level: i,
-      format: e.combine(e.errors({ stack: !0 }), e.prettyPrint())
-    }),
+    // new transports.Console({
+    //     level: logLevel,
+    //     format: format.combine(format.errors({ stack: true }), format.prettyPrint())
+    // }),
     r
   ],
   exceptionHandlers: [r],
