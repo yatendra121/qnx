088ce2ea594613ee99b753cdedffe49b8ef378d6
Yatendra Kushwaha <yatendra121kushwaha@gmail.com>
2024-03-05 17:13:44 +0530

    fix: vite config

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -55,7 +55,7 @@ export function collectViteConfig(packageJson: any, dirName: string, option = {}
             cache: {
                 dir: '../../node_modules/.vitest'
             },
-            environment: 'jsdom',
+            environment: 'node',
             include: [`src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}`]
         }
         // resolve: {
