d1ed2ee585d44e3cc51fa6f548e3117860d248ce
Yatendra Kushwaha <yatendra121kushwaha@gmail.com>
2024-05-23 18:18:04 +0530

    feat(log): improved comments for code editor intellisense

--- packages/log/src/lib/log.ts
+++ packages/log/src/lib/log.ts
@@ -1,11 +1,20 @@
 import chalk from 'chalk'
 
+/**
+ * Type representing the possible color names for log messages.
+ */
 type ColorName = 'red' | 'green' | 'cyan' | 'yellow'
 
+/**
+ * Type representing the possible types of log messages.
+ */
 type Type = 'info' | 'error' | 'success' | 'warning'
 
 const log = console.log
 
+/**
+ * Configuration object mapping each log type to a corresponding color.
+ */
 const config: Record<Type, ColorName> = {
     info: 'cyan',
     error: 'red',
@@ -13,6 +22,16 @@ const config: Record<Type, ColorName> = {
     warning: 'yellow'
 }
 
+/**
+ * Logs a message to the console with a specific color based on the type.
+ *
+ * @param message - The message to be logged.
+ * @param options - An object containing the type of the message. Defaults to 'info'.
+ *
+ * @example
+ * consoleLog('This is an info message', { type: 'info' })
+ * consoleLog('This is an error message', { type: 'error' })
+ */
 export function consoleLog(message: string, { type }: { type: Type } = { type: 'info' }): void {
     log(chalk[config[type]](message))
 }
