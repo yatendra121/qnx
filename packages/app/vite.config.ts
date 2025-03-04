556734bedf2af815ac529eedf27675bd5f854eb5
yatendra121 <yatendra121kushwaha@gmail.com>
2025-03-04 17:59:56 +0530

    feat(app): changed unit testing lib to vitest

---
+++ packages/app/vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
+
+export default defineConfig({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/packages/app',
+    plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    test: {
+        watch: false,
+        globals: true,
+        environment: 'jsdom',
+        include: ['__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        reporters: ['default'],
+        coverage: {
+            reportsDirectory: '../../coverage/packages/app',
+            provider: 'v8'
+        }
+    }
+})
